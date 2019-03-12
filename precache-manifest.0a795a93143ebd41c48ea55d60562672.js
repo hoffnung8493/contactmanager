6fde1268ede06b6eeac9172563e15849e055202e
DESKTOP-R96KQH6\Sihoon Kim <hoffnung8493@kaist.ac.kr>
2019-03-12 12:48:11 +0900

    Updates

---
+++ precache-manifest.0a795a93143ebd41c48ea55d60562672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "32f68240a56328fae25c",
+    "url": "/contactmanager/static/js/main.32f68240.chunk.js"
+  },
+  {
+    "revision": "4d3d4bd3234f8ef04528",
+    "url": "/contactmanager/static/js/2.4d3d4bd3.chunk.js"
+  },
+  {
+    "revision": "32f68240a56328fae25c",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "4d3d4bd3234f8ef04528",
+    "url": "/contactmanager/static/css/2.ff306c91.chunk.css"
+  },
+  {
+    "revision": "5e5d2646df05d27e3c99b160504f4c8f",
+    "url": "/contactmanager/index.html"
+  }
+];
